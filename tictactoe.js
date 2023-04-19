4b422124eb68d31876c030bbb7db05a76cffb771
jeremyHarrell <hillvulf@gmail.com>
2023-04-19 12:09:10 -0500

    create tictactoe.js and begin first function

---
+++ tictactoe.js
@@ -0,0 +1,7 @@
+
+let getComputerChoice = (choices) => {
+  //create three options: rock, paper and scissors
+  let choices = [rock, paper, scissors]
+  // have this function randomly select one each time it is called
+  // then return the selected option
+}
