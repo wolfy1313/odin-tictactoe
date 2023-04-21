3b951940b05afa4c2ede098288e091ec8a84ac2e
jeremyHarrell <hillvulf@gmail.com>
2023-04-21 14:48:20 -0500

    test getComputerChoice function

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,9 @@
 
+const choices = ["rock", "paper", "scissors"]
 let getComputerChoice = (choices) => {
-  //create three options: rock, paper and scissors
-  let choices = [rock, paper, scissors]
-  // have this function randomly select one each time it is called
-  // then return the selected option
+  return choices[Math.floor(Math.random()*choices.length)]
 }
+console.log(getComputerChoice(choices))
+//create three options: rock, paper and scissors
+// have this function randomly select one each time it is called
+// then return the selected option
