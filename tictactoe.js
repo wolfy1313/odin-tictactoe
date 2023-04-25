a7d6ab846bd6f1f180289282bc99efbba69c5b77
jeremyHarrell <hillvulf@gmail.com>
2023-04-25 10:50:00 -0500

    odin tic tac toe switch statement working

--- tictactoe.js
+++ tictactoe.js
@@ -2,23 +2,54 @@
 //create three options: rock, paper and scissors
 // have this function randomly select one each time it is called
 // then return the selected option
-const choices = ["rock", "paper", "scissors"]
-let getComputerChoice = choices => choices[Math.floor(Math.random()*choices.length)]
 // console.log(getComputerChoice(choices))
+const choices = ["rock", "paper", "scissors"]
+const getComputerChoice = choices => choices[Math.floor(Math.random()*choices.length)]
 
 let playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === "rock" && computerSelection === "rock"){
-    return "It's a tie!"
-  } else if (playerSelection === "rock" && computerSelection === "scissors"){
-    return "Computer selected scissors, you WIN!"
-  } else {
-    return "Computer selected paper, you LOSE!"
+  switch(playerSelection.toLowerCase()){
+    case 'rock':
+      if (computerSelection === 'rock') {
+        return "it is a tie"
+      }
+      else if (computerSelection === 'paper'){
+        return "you lose computer picked paper"
+      }
+      else if (computerSelection === 'scissors'){
+        return "you win computer selected scissor"
+      }
+      break;
+    case 'paper':
+      if (computerSelection === 'paper') {
+        return "it is a tie"
+      }
+      else if (computerSelection === 'scissors'){
+        return "you lose computer picked scissors"
+      }
+      else if (computerSelection === 'rock'){
+        return "you win computer selected rock"
+      }
+      break;
+    case 'scissors':
+      if (computerSelection === 'scissors') {
+        return "it is a tie"
+      }
+      else if (computerSelection === 'rock'){
+        return "you lose computer picked rock"
+      }
+      else if (computerSelection === 'paper'){
+        return "you win computer selected paper"
+      }
+      break;
+    default:
+      return "that is not a valid selection"
   }
 }
+// const getPlayerChoice = prompt("Choose paper, rock or scissors")
 
-const playerSelection = "rock"
+const playerSelection = prompt("Choose paper, rock or scissors");
 const computerSelection = getComputerChoice(choices)
-console.log(playRound("rock", getComputerChoice(choices)))
+console.log(playRound(playerSelection, getComputerChoice(choices)))
 
 // let playerSelection = () => {
 
